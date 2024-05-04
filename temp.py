c3555a3bf6ed486a65c5d26390d992cdbc7b029f
Roby Lombardo <5968800-RobyLombardo@users.noreply.replit.com>
2024-05-04 05:19:25 +0000

    checking AI solution

--- temp.py
+++ temp.py
@@ -2,13 +2,37 @@ from pathlib import Path
 
 flat = {}
 
-for path in Path(".").glob("**/*"):
-    # print(path)
-
+def store_flat(flat:dict, path:Path):
     if not path.parts[0].startswith('.'):
         if path.is_file():
             parent = str(path.parent)
             flat[parent] = flat.get(parent, [])
             flat[parent].append(path.name)
 
+nested = {}
+def store_nested(nested:dict, parts:tuple):
+    '''
+    BASE_CASE:  length of parts is 1
+                
+    RECURSIVE_CASE: length of parts is > 1
+                    check if parts[0] is in nested
+                    if not add new key (parts[0])
+                        add new value, empty list
+                    else
+                        append
+    '''
+    parent = parts[0]
+    if len(parts) > 1:
+        nested[parent] = nested.get(parent, {})
+        store_nested(nested[parent], parts[1:])
+    else:
+        nested[parent] = nested.get(parent, [])
+        nested[parent].append(parts[0])
+
+for path in Path(".").glob("**/*"):
+    store_flat(flat, path)
+
+for path in Path(".").glob("**/*"):
+    store_nested(nested, path.parts)
 print(flat)
+print(nested)
